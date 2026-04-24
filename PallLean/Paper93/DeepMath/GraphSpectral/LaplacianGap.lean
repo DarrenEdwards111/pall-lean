39a542df1ff9116f09e9b783a033e2ee886c1918
Darren Edwards <darrenedwards111@gmail.com>
2026-04-24 13:30:24 +0100

    Add LaplacianGap kernel-only stub for graph spectral deep math

---
+++ PallLean/Paper93/DeepMath/GraphSpectral/LaplacianGap.lean
@@ -0,0 +1,9 @@
+import Mathlib.Data.Real.Basic
+
+namespace PallLean.Paper93.DeepMath.GraphSpectral
+
+def spectralGap : ℝ := 0
+
+theorem spectralGap_nonneg : 0 ≤ spectralGap := le_refl _
+
+end PallLean.Paper93.DeepMath.GraphSpectral
