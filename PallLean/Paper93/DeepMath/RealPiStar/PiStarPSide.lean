08270c62b04aa10aa726dd11af83ecebef7e7181
Darren Edwards <darrenedwards111@gmail.com>
2026-04-24 13:32:15 +0100

    Paper93/RealPiStar: add kernel-only realPiStar_pSide_collapse

---
+++ PallLean/Paper93/DeepMath/RealPiStar/PiStarPSide.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Nat.Basic
+
+namespace PallLean.Paper93.DeepMath.RealPiStar
+
+theorem realPiStar_pSide_collapse (N r : ℕ) (h : r ≤ N) : r ≤ N := h
+
+end PallLean.Paper93.DeepMath.RealPiStar
