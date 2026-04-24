3df61f13ed79e9629e3d46e85f734e8ed74e06a6
Darren Edwards <darrenedwards111@gmail.com>
2026-04-24 13:32:05 +0100

    Add BridgeB.GlobalRank global_rank_bound stub

---
+++ PallLean/Paper93/DeepMath/BridgeB/GlobalRank.lean
@@ -0,0 +1,7 @@
+import Mathlib.Data.Real.Basic
+
+namespace PallLean.Paper93.DeepMath.BridgeB
+
+theorem global_rank_bound (n : ℕ) (s : ℕ) (δ : ℝ) (hδ : 0 < δ) (hs : s ≤ n) : s ≤ n := hs
+
+end PallLean.Paper93.DeepMath.BridgeB
