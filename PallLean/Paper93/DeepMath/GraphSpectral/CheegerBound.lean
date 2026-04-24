9aa191b57cf9ff2c9f4ed209339b2367e8c0287d
Darren Edwards <darrenedwards111@gmail.com>
2026-04-24 13:30:31 +0100

    Add kernel-only CheegerBound stub in GraphSpectral

---
+++ PallLean/Paper93/DeepMath/GraphSpectral/CheegerBound.lean
@@ -0,0 +1,9 @@
+import Mathlib.Data.Real.Basic
+
+namespace PallLean.Paper93.DeepMath.GraphSpectral
+
+def conductance : ℝ := 0
+
+theorem cheegerBound : 0 ≤ conductance := le_refl _
+
+end PallLean.Paper93.DeepMath.GraphSpectral
