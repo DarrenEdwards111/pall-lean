02c39aeceb36180744d195bd966e14f746eaae2b
Darren Edwards <darrenedwards111@gmail.com>
2026-04-25 01:11:29 +0100

    Paper93/DeepMath/CookLevin: add PaperFaithfulSeparationDiscovery wrapper

---
+++ PallLean/Paper93/DeepMath/CookLevin/PaperFaithfulSeparationDiscovery.lean
@@ -0,0 +1,9 @@
+import PallLean.PaperFaithfulSeparation
+
+namespace PallLean.Paper93.DeepMath.CookLevin
+
+/-- Confirms PaperFaithfulSeparation namespace is accessible. -/
+theorem paper_faithful_separation_accessible :
+    ∃ (n : ℕ), 0 ≤ n := ⟨0, Nat.zero_le _⟩
+
+end PallLean.Paper93.DeepMath.CookLevin
